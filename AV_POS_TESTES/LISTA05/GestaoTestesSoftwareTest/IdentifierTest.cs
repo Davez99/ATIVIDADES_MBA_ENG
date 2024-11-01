adeadbdfde3ac1e3d7750b903112bf4eb6f31610
Davi Terres da Silva <davi.silva@COLOMBO.PLANSUL>
2024-11-01 11:38:06 -0300

    av_testes_lista05

---
+++ AV_POS_TESTES/LISTA05/GestaoTestesSoftwareTest/IdentifierTest.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GestaoTestesSoftwareTest
+{
+    public class IdentifierTest
+    {
+
+        /*
+            Camada de teste tem por objetivo criar testes de validação para a classe Identifier 
+        */
+
+        [Fact]
+        public void Test1()
+        {
+
+
+
+            Assert.True(true);
+        }
+    }
+}
